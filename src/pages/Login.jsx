5973f4bf52ecde484ff8a50ea79eaa4a1b6fb5cb
rayir03 <rayir03@gmail.com>
2025-03-08 16:04:01 -0600

    Carpetas orden

---
+++ src/pages/Login.jsx
@@ -0,0 +1,16 @@
+import styled from "styled-components";
+
+const Login = () => {
+  return (
+    <Container>
+      <h1>Componente</h1>
+    </Container>
+  )
+}
+
+
+const Container = styled.div``
+
+
+
+export default Login
