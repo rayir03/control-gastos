579db62730a82b9f6bd3b7626ddee255a2db9633
rayir03 <rayir03@gmail.com>
2025-03-17 21:21:14 -0500

    style Sidebar icon links

--- src/components/organismos/sidebar/Sidebar.jsx
+++ src/components/organismos/sidebar/Sidebar.jsx
@@ -5,30 +5,30 @@ import { NavLink } from "react-router-dom";
 export function Sidebar({ state, setState }) {
   return (
     <Main>
-      <Container isOpen={ state }>
+      <Container isOpen={state}>
         <div className="Logocontent">
           <div className="imgcontent">
-            <img src={v.logo} alt="logo"/>
+            <img src={v.logo} alt="logo" />
           </div>
           <h2>Gastos</h2>
-
         </div>
-        {
-          LinksArray.map(({ icon, label, to }) => (
-            <NavLink to={ to }>
-              <div className="Linkicon">{ icon }</div>
-              <span>{ label }</span>
+        {LinksArray.map(({ icon, label, to }) => (
+          <div
+            key={label}
+            className={state ? "LinkContainer active" : "LinkContainer"}
+          >
+            <NavLink to={to} className={({ isActive }) => `Links${isActive?` active` : ``}`}>
+              <div className="Linkicon">{icon}</div>
+              <span>{label}</span>
             </NavLink>
-          ))
-        }
+          </div>
+        ))}
         <Divider />
       </Container>
-
     </Main>
-  )
+  );
 }
 
- 
 const Container = styled.div`
   color: ${(props) => props.theme.text};
   background: ${(props) => props.theme.bg};
@@ -47,16 +47,16 @@ const Container = styled.div`
       align-items: center;
       width: 30px;
       cursor: pointer;
-      transition: all  0.5s ease-in-out;
-      transform: ${({ isOpen }) => (isOpen ? `scale(0.7)` : 
-        `scale(1.5)`)} rotate(${(props) => props.theme.logorotate});
-        img {
-         width: 100%;
-         animation: flotar 1.7s ease-in-out infinite alternate; 
-        }
+      transition: all 0.5s ease-in-out;
+      transform: ${({ isOpen }) => (isOpen ? `scale(0.7)` : `scale(1.5)`)}
+        rotate(${(props) => props.theme.logorotate});
+      img {
+        width: 100%;
+        animation: flotar 1.7s ease-in-out infinite alternate;
+      }
     }
     h2 {
-      display: ${({ isOpen }) => (isOpen ? `block` : `none`)};  
+      display: ${({ isOpen }) => (isOpen ? `block` : `none`)};
     }
     @keyframes flotar {
       0% {
@@ -70,16 +70,54 @@ const Container = styled.div`
       }
     }
   }
+  .LinkContainer {
+    margin: 5px 0;
+    transition: all 0.3s;
+    padding: 0 5%;
+    position: relative;
+    &:hover {
+      background: ${(props) => props.theme.bgAlpha};
+    }
+    .Links {
+      display: flex;
+      align-items: center;
+      text-decoration: none;
+      padding: calc(${()=> v.smSpacing} - 2px) 0;
+      color: ${(props) => props.theme.text};
+      height: 60px;
+      .Linkicon {
+        padding: ${() => v.smSpacing} ${() => v.mdSpacing};
+        display: flex;
+        svg {
+          font-size: 25px;
+        }
+      }
+      &.active {
+        color: ${(props) => props.theme.bg5};
+        &::before {
+          content: "";
+          position: absolute;
+          height: 100%;
+          background: ${(props) => props.theme.bg5};
+          width: 4px;
+          border-radius: 10px;
+          left: 0;
+        }
+        
 
-`
+      }
+    }
+  }
+`;
 
-const Main = styled.div`
-
-`
+const Main = styled.div``;
 
 const Divider = styled.div`
   height: 1px;
   width: 100%;
   background: ${(props) => props.theme.bg4};
-  margin: ${() => {return v.lgSpacing}} 0;
-`
+  margin: ${() => {
+      return v.lgSpacing;
+    }}
+    0;
+`;
