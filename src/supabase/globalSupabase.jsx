90f1794f54e578346568506866375084d028546d
rayir03 <rayir03@gmail.com>
2025-03-23 22:00:11 -0500

    trabajando con supabase

---
+++ src/supabase/globalSupabase.jsx
@@ -0,0 +1,12 @@
+import { supabase } from "../index";
+
+const ObtenerIdAuthSupabase = async () => {
+    const { data: {session}} = await supabase.auth.getSession();
+
+    if(session != null) {
+        const { user } = session;
+        const idAuthSupabase = user.id;
+        
+        return idAuthSupabase;
+    }
+}
